2a882b3fe359675a7b9f222a19f89a0a502ee932
Calileus <79671075+Calileus@users.noreply.github.com>
2026-09-13 11:25:38 -0600

    Revert "Migrating to multi-page approach"

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
-export default defineConfig({
-  site: "https://calileus.github.io",
-  integrations: [tailwind()]
-});
